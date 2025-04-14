cc9095be46e08897b38d533b277e89d0e5bb82d4
av6387 <98997680+av6387@users.noreply.github.com>
2025-04-14 16:15:53 +0000

    modified:   script/main.js

--- script/main.js
+++ script/main.js
@@ -1,13 +1,9 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
+        title: '💚🫂❤️',
         confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
         confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
